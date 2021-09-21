caac33b6eaa8c731494926cdec51523e6c65b56a
Leonard <57763635+Snowzter@users.noreply.github.com>
2021-09-21 19:04:10 +0200

    Create openimage.py

---
+++ openimage.py
@@ -0,0 +1,11 @@
+#install pip if u havn't
+#pip install Pillow (in the terminal)
+
+from PIL import Image
+
+im = Image.open(r"location of the picture")
+
+
+# Remove the line under me if u don't want to open it multiple times.
+for x in range(how many times u want to repeat it):
+    im.show()
